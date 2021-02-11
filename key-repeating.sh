17d6c741f438add003ed230def0d4fb207a3611a
Daniel Lee <54050413+dlee100@users.noreply.github.com>
2021-02-11 00:13:33 -0500

    added script and txt

---
+++ key-repeating.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# this is a script enable navigation key repeating on vscode for mac
+
+defaults write com.microsoft.VSCode ApplePressAndHoldEnabled -bool false
+defaults write com.microsoft.VSCodeInsiders ApplePressAndHoldEnabled -bool false
+defaults write com.visualstudio.code.oss ApplePressAndHoldEnabled -bool false
+defaults delete -g ApplePressAndHoldEnabled
